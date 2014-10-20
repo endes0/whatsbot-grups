ef941fc8c900a17739514cf16ed72bcce352c6b4
endes123321 <endes123321@gmail.com>
2014-10-20 17:45:15 +0200

    trabajando en las funciones
    he trabajado un poco en las funciones corrigiendo errores y
    perfeccionandolas mañana ya termino algunas cosas y sigo con lo demas

--- funciones.php
+++ funciones.php
@@ -41,6 +41,8 @@ $w->sendMessage($Gnumero, "porfavor escriba numeros de tlf.");
 else {
 unset($comando[0]);
 $w->sendGroupsParticipantsAdd($Gnumero, $comando);
+sleep(2);
+$w->sendMessage($grupo, "		comandos		\n/hola -te saluda. \n/add [numeros de telefonos separados por espacios] -añade participantes. \n/remove [numeros de telefonos separados por espacios] -quita participantes participantes.");
 }
 }
 else if ($comando[0]=='remove') {
@@ -50,7 +52,34 @@ $w->sendMessage($Gnumero, "porfavor escriba numeros de tlf.");
 else {
 unset($comando[0]);
 $w->sendGroupsParticipantsRemove($Gnumero, $comando);
+$w->sendMessage($Gnumero, "usuario/s eliminado/s del grupo");
 }
+}
+}
+function CrearGrupo($asunto, $participantes)
+{
+global $w;
+$grupo = $w->sendGroupsChatCreate($asunto, $participantes, $creador);
+$w->sendMessage($grupo, "bienvenid@s al grupo '".$asunto."' creado por '".$creador."' \nusando Whatsbot by endes3000");
+$w->sendMessage($grupo, "		comandos		\n/hola -te saluda. \n/add [numeros de telefonos separados por espacios] -añade participantes. \n/remove [numeros de telefonos separados por espacios] -quita participantes participantes.");
+}
+function AbGrupo($Gnumero, $Gjid, $participantes, $tipo)
+{
+global $w;
+if ($tipo=='AB') {
+$w->sendMessage($Gnumero, "se ha recibido unapeticion de eliminacion des este grupo \nel grupo se va a eliminar dentro de 20 segundos");
+sleep(10);	
+$w->sendMessage($Gnumero, "el grupo se va ha eliminar dentro de 10 segundos");
+sleep(5);
+$w->sendMessage($Gnumero, "el grupo se va a eliminar dentro de 5 segundos");
+sleep(5);
+$w->sendGroupsParticipantsRemove($Gnumero, $participantes);
+sleep(1);
+$w->sendGroupsChatEnd($Gjid);
+}
+else {
+$w->sendMessage($Gnumero, "se ha recibido una peticion para que whats bot deje de administrar este grupo \nadios.");
+$w->sendGroupsChatEnd($Gjid);	
 }
 }
 ?>
